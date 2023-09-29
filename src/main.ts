72d565fef1f0f53ab51e3350bc9f3bff62cd053c
栗嘉男 <574980606@qq.com>
2023-09-30 03:03:36 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+};
+
+bootstrap();
