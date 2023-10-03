203ec05c99b159a6420e5bc2f261eba410f946eb
栗嘉男 <574980606@qq.com>
2023-10-03 08:12:18 +0800

    fix: fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import type { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
 
+const options: TypeOrmModuleOptions = {
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "pass123",
+  database: "postgres",
+  autoLoadEntities: true,
+  synchronize: true,
+};
+
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(options)],
   controllers: [AppController],
   providers: [AppService],
 })
