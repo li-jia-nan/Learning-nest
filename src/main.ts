c2242dec2d5e15a3579f50632e401db34603b465
栗嘉男 <574980606@qq.com>
2024-07-06 05:23:37 +0800

    demo: demo update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
-}
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+};
 
 bootstrap();
