897311fd56330e105c248102d6ffc385bd747c4d
栗嘉男 <574980606@qq.com>
2024-07-07 03:21:17 +0800

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from './enum/config.enum';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getUsers() {
+    this.configService.get(ConfigEnum.DB);
     return this.userService.getUsers();
   }
 
